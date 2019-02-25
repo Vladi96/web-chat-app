16ab30fa1cbceb277de6aa9bd91f6800112e4c14
Vladi96 <vladika96@abv.bg>
2019-02-26 01:44:38 +0200

    Create Chat Project!

---
+++ Node-chat-app/server/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+const publicPath = path.join(__dirname,'../public');
+
+app.use(express.static(publicPath));
+
+app.listen(port);
